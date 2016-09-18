aa82f851ec7fc94bfa8540f6e0a79a7b853dd87e
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-09-17 20:14:30 -0500

    Add DetailActivity
    - The detail activity will show details about each individual event.

---
+++ app/src/main/java/com/duse/android/dsmsocialclub/activity/DetailActivity.java
@@ -0,0 +1,19 @@
+package com.duse.android.dsmsocialclub.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mahesh Gaya on 9/17/16.
+ */
+
+//TODO: This Activity will show each individual events
+    //So when you tap on the event this activity will be triggered
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
