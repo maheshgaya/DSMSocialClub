688e8ba76dff3c8054390a86da297c1c86f5e433
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-09-18 21:12:44 -0500

    Add card for event

--- app/src/main/java/com/duse/android/dsmsocialclub/activity/SocialActivity.java
+++ app/src/main/java/com/duse/android/dsmsocialclub/activity/SocialActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 
