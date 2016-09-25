59a4174fb45f0b5165c870d6d42182c9bf848d69
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-09-24 20:28:44 -0500

    Implement SharedPreferences for HomeFragment

--- app/src/main/java/com/duse/android/dsmsocialclub/fragment/HomeFragment.java
+++ app/src/main/java/com/duse/android/dsmsocialclub/fragment/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.duse.android.dsmsocialclub.fragment;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,7 +21,7 @@ import com.duse.android.dsmsocialclub.model.EventModel;
 
 import java.util.List;
 
-public class HomeFragment extends Fragment{
+public class HomeFragment extends Fragment {
     private EventModel[] mEvents = {}; //store events
     private EventAdapter mEventAdapter; //use for recycleview/cardview
     private RecyclerView mRecycleView; //use for UI
@@ -34,16 +37,53 @@ public class HomeFragment extends Fragment{
     public void onStart() {
         super.onStart();
         //if array is zero, get data
-        if (mEvents.length == 0) {
-            updateEvents();
-        }
+
+        updateEvents();
+
     }
 
     public void updateEvents(){
         //get event updates
-        FetchEventTask fetchEventTask = new FetchEventTask();
-        //TODO: Add params for sorting, shared preferences, if have time
-        fetchEventTask.execute();
+        //This works Woohoo
+        FetchEventInterestsTask fetchEventTask = new FetchEventInterestsTask();
+        Boolean[] preferences = new Boolean[15];
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        Boolean prefCircus = prefs.getBoolean(getString(R.string.interest_circus_value), true);
+        preferences[0] = prefCircus;
+        Boolean prefComedy = prefs.getBoolean(getString(R.string.interest_comedy_value), true);
+        preferences[1] = prefComedy;
+        Boolean prefCulinary = prefs.getBoolean(getString(R.string.interest_culinary_value), true);
+        preferences[2] = prefCulinary;
+        Boolean prefDanceMovement = prefs.getBoolean(getString(R.string.interest_dance_movement_value), true);
+        preferences[3] = prefDanceMovement;
+        Boolean prefEducation = prefs.getBoolean(getString(R.string.interest_education_value), true);
+        preferences[4] = prefEducation;
+
+        Boolean prefFeaturedEvent = prefs.getBoolean(getString(R.string.interest_featured_event_value), true);
+        preferences[5] = prefFeaturedEvent;
+        Boolean prefFilm = prefs.getBoolean(getString(R.string.interest_film_value), true);
+        preferences[6] = prefFilm;
+        Boolean prefKids = prefs.getBoolean(getString(R.string.interest_kids_value), true);
+        preferences[7] = prefKids;
+        Boolean prefLifestyle = prefs.getBoolean(getString(R.string.interest_lifestyle_value), true);
+        preferences[8] = prefLifestyle;
+        Boolean prefLiterary = prefs.getBoolean(getString(R.string.interest_literary_value), true);
+        preferences[9] = prefLiterary;
+
+        Boolean prefMusic = prefs.getBoolean(getString(R.string.interest_music_value), true);
+        preferences[10] = prefMusic;
+        Boolean prefSocial = prefs.getBoolean(getString(R.string.interest_social_value), true);
+        preferences[11] = prefSocial;
+        Boolean prefTechnology = prefs.getBoolean(getString(R.string.interest_technology_value), true);
+        preferences[12] = prefTechnology;
+        Boolean prefTheater = prefs.getBoolean(getString(R.string.interest_theater_value), true);
+        preferences[13] = prefTheater;
+        Boolean prefVisualArt = prefs.getBoolean(getString(R.string.interest_visual_art_value), true);
+        preferences[14] = prefVisualArt;
+
+
+        fetchEventTask.execute(preferences);
     }
 
 
@@ -72,8 +112,9 @@ public class HomeFragment extends Fragment{
     }
 
 
-    private class FetchEventTask extends AsyncTask<Void, Void, EventModel[]> {
-        private final String LOG_TAG = FetchEventTask.class.getSimpleName();
+
+    private class FetchEventInterestsTask extends AsyncTask<Boolean, Void, EventModel[]> {
+        private final String LOG_TAG = FetchEventInterestsTask.class.getSimpleName();
 
         @Override
         protected void onPostExecute(EventModel[] result) {
@@ -94,10 +135,15 @@ public class HomeFragment extends Fragment{
         }
 
         @Override
-        protected EventModel[] doInBackground(Void... voids) {
+        protected EventModel[] doInBackground(Boolean... params) {
+            //Log.d(LOG_TAG, "doInBackground: " + params.length);
+            if (params.length == 0){
+                return null;
+            }
             //get events from json
-            List<EventModel> eventList = new EventJsonGetter(getActivity()).getEventsList();
+            List<EventModel> eventList = new EventJsonGetter(getActivity()).getEventsList(params);
             EventModel[] events = eventList.toArray(new EventModel[eventList.size()]);
+            Log.d(LOG_TAG, "doInBackground: " + events.toString());
             return events;
         }
     }
--- app/src/main/java/com/duse/android/dsmsocialclub/model/EventJsonGetter.java
+++ app/src/main/java/com/duse/android/dsmsocialclub/model/EventJsonGetter.java
@@ -2,8 +2,10 @@ package com.duse.android.dsmsocialclub.model;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
 import com.duse.android.dsmsocialclub.Constant;
+import com.duse.android.dsmsocialclub.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,6 +14,8 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * EventJsonGetter
@@ -21,7 +25,7 @@ import java.util.ArrayList;
 public class EventJsonGetter {
     private ArrayList<EventModel> eventsList;
     private Context mContext;
-
+    private final String TAG = EventJsonGetter.class.getSimpleName();
 
     /**
      * Construct for EventJsonGetter
@@ -39,7 +43,7 @@ public class EventJsonGetter {
     public ArrayList<EventModel> getEventsList(){
         //initializing
         eventsList = new ArrayList<>();
-        String eventJson = null;
+        String eventJson;
 
         //read from file, and put to a buffer
         try{
@@ -72,10 +76,71 @@ public class EventJsonGetter {
                 String description = eventObject.getString(Constant.EVENT_DESCRIPTION);
                 String imageUrl = eventObject.getString(Constant.EVENT_IMAGE_URL);
                 String location = eventObject.getString(Constant.EVENT_LOCATION);
-
+                Log.d(TAG, "getEventsList: " + interest);
                 EventModel event = new EventModel(id, title, description, date, time, location, imageUrl, interest);
 
                 eventsList.add(event);
+            }
+
+        }catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+
+
+        return eventsList;
+    }
+
+    public ArrayList<EventModel> getEventsList(Boolean[] interestsArray){
+        //initializing
+        eventsList = new ArrayList<>();
+        String eventJson;
+        List interestsList = Arrays.asList(mContext.getResources().getStringArray(R.array.interests_values));
+
+        //read from file, and put to a buffer
+        try{
+            InputStream inputStream = mContext.getAssets().open("events.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+
+            inputStream.read(buffer);
+            inputStream.close();
+            eventJson = new String(buffer, "UTF-8");
+
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        int position = 0;
+        //read the json from the buffer
+        try {
+            JSONObject eventsJsonObject = new JSONObject(eventJson);
+            JSONArray eventJsonArray = eventsJsonObject.getJSONArray("events");
+
+            for (int i = 0; i < eventJsonArray.length(); i++){
+                JSONObject eventObject = eventJsonArray.getJSONObject(i);
+                //In real case, id should be a unique number
+                int id = eventObject.getInt(Constant.EVENT_ID);
+                String title = eventObject.getString(Constant.EVENT_TITLE);
+                String date = eventObject.getString(Constant.EVENT_DATE);
+                String time = eventObject.getString(Constant.EVENT_TIME);
+                String interest = eventObject.getString(Constant.EVENT_INTEREST);
+                String description = eventObject.getString(Constant.EVENT_DESCRIPTION);
+                String imageUrl = eventObject.getString(Constant.EVENT_IMAGE_URL);
+                String location = eventObject.getString(Constant.EVENT_LOCATION);
+
+                //check if the interest exists, get the position
+                if (interestsList.contains(interest)){
+                    position = interestsList.indexOf(interest);
+                }
+
+                //check if the interest in preferences is checked by user, then add to list
+                if (interestsArray[position]){
+                    EventModel event = new EventModel(id, title, description, date, time, location, imageUrl, interest);
+                    eventsList.add(event);
+                }
+
             }
 
         }catch (JSONException e){
