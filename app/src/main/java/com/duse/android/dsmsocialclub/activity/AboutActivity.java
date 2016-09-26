27de2e04569dce364b81304e17415b95f4c41778
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-09-26 09:28:53 -0500

    Add About page

--- app/src/main/java/com/duse/android/dsmsocialclub/activity/AboutActivity.java
+++ app/src/main/java/com/duse/android/dsmsocialclub/activity/AboutActivity.java
@@ -4,11 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.duse.android.dsmsocialclub.R;
+import com.duse.android.dsmsocialclub.fragment.AboutFragment;
+
 //@assignee: Henry
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setTitle(getResources().getString(R.string.about_label));
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container_about, new AboutFragment())
+                    .commit();
+        }
     }
+
 }
--- app/src/main/java/com/duse/android/dsmsocialclub/fragment/AboutFragment.java
+++ app/src/main/java/com/duse/android/dsmsocialclub/fragment/AboutFragment.java
@@ -1,5 +1,23 @@
 package com.duse.android.dsmsocialclub.fragment;
 
-//@assignee: Henry
-public class AboutFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.duse.android.dsmsocialclub.R;
+
+public class AboutFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        setRetainInstance(true);
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        return rootView;
+
+    }
 }
